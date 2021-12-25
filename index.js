c13a640b5f9424225c72d39da1200fc28e82548c
Hongsoon Kim <khsun6226@naver.com>
2021-12-25 03:48:12 -0800

    retrieves user data and sends it to client

--- index.js
+++ index.js
@@ -164,43 +164,77 @@ app.get("/getnames", (req, res) => {
 });
 
 app.get("/getplayerscore", (req, res) => {
+  const playerArr = [];
   db.query(
-    "SELECT userId FROM user WHERE name = ?",
-    req.query.name,
-    (err, result) => {
+    "SELECT DISTINCT name FROM user INNER JOIN matchplayer ON user.userId = matchplayer.playerId ORDER BY name ASC",
+    (err, result1) => {
       if (err) {
         console.log("err", err);
       } else {
-        console.log("result", result);
-        db.query(
-          "SELECT winCount FROM matchplayer WHERE playerId = ?",
-          result[0].userId,
-          (err, result) => {
-            if (err) {
-              console.log("err", err);
-            } else {
-              const map = result.map((obj) => {
-                return obj.winCount;
-              });
-              const reducer = (acc, cur) => {
-                console.log("acc", acc);
-                console.log("cur", cur);
-                return acc + cur;
-              };
-              const wins = map.reduce(reducer);
-              const partPoint = result.length * 10;
-              const winPoint = wins * 10;
-              res.send({
-                result: {
-                  name: req.query.name,
-                  partPoint: partPoint,
-                  winPoint: winPoint,
-                  total: partPoint + winPoint,
-                },
-              });
+        console.log("result1", result1);
+        for (let i = 0; i < result1.length; i++) {
+          db.query(
+            "SELECT userId FROM user WHERE name = ?",
+            result1[i].name,
+            (err, result2) => {
+              if (err) {
+                console.log("err", err);
+              } else {
+                console.log("result2", result2);
+                db.query(
+                  "SELECT winCount FROM matchplayer WHERE playerId = ?",
+                  result2[0].userId,
+                  (err, result3) => {
+                    if (err) {
+                      console.log("err", err);
+                    } else {
+                      console.log("result3", result3);
+                      const map = result3.map((obj) => {
+                        return obj.winCount;
+                      });
+                      const reducer = (acc, cur) => {
+                        console.log("acc", acc);
+                        console.log("cur", cur);
+                        return acc + cur;
+                      };
+                      const wins = map.reduce(reducer);
+                      const partPoint = result3.length * 10;
+                      const winPoint = wins * 10;
+                      playerArr.push({
+                        name: result1[i].name,
+                        partPoint: partPoint,
+                        winPoint: winPoint,
+                        total: partPoint + winPoint,
+                      });
+
+                      // res.send({
+                      //   result: {
+                      //     name: req.query.name,
+                      //     partPoint: partPoint,
+                      //     winPoint: winPoint,
+                      //     total: partPoint + winPoint,
+                      //   },
+                      // });
+
+                      // res.write(
+                      //   JSON.stringify({
+                      //     name: player.name,
+                      //     partPoint: partPoint,
+                      //     winPoint: winPoint,
+                      //     total: partPoint + winPoint,
+                      //   })
+                      // );
+                    }
+                    console.log("arr", playerArr);
+                    if (i === result1.length - 1) {
+                      res.send(JSON.stringify(playerArr));
+                    }
+                  }
+                );
+              }
             }
-          }
-        );
+          );
+        }
       }
     }
   );
