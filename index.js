5bba8dbdae189fdbe03c4cc347c9c334f7684f8c
Hongsoon Kim <khsun6226@naver.com>
2021-12-25 17:21:37 -0800

    change cors policy

--- index.js
+++ index.js
@@ -16,12 +16,11 @@ require("dotenv").config();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: "*",
     methods: ["GET", "POST"],
-    credentials: true,
   })
 );
-app.use(cors());
+// app.use(cors());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 
